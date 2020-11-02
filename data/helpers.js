7431d49a96c2f47b5ec35b03cc23d483feb93020
gennadygortalov <ggortalov@yahoo.com>
2020-11-02 00:15:32 -0500

    improved and organized

---
+++ data/helpers.js
@@ -0,0 +1,25 @@
+const field = {
+    name: 0,
+    gender: 1,
+    age: 2,
+    story: 3,
+    image: 4,
+}
+const gender = {
+    HE: 0,
+    SHE: 1,
+    IT: 2,
+}
+const story = {
+    overcomingTheMonster: 0,
+    rebirth: 1,
+    quest: 2,
+    journeyAndReturn: 3,
+    ragsAndRiches: 4,
+    tragedy: 5,
+    comedy: 6,
+}
+
+
+
+module.exports = { field, gender, story};
