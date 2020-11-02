aab9093d21bbf7af7fe7b25d3c187512f2bd4d74
Andrii-KV <andrii.kovalev1@gmail.com>
2020-11-01 17:04:20 -0800

    added:TC, selectors, helpers, expected

---
+++ data/helpers.js
@@ -0,0 +1,21 @@
+const field = {
+    name: 0,
+    gender: 1,
+    HE: 0,
+    SHE: 1,
+    IT: 2,
+    age: 2,
+    story: 3,
+    image: 4,
+    overcomingTheMonster: 0,
+    rebirth: 1,
+    quest: 2,
+    journeyAndReturn: 3,
+    ragsAndRiches: 4,
+    tragedy: 5,
+    comedy: 6,
+    imageFile: "C:/2.jpg"
+
+}
+
+export default field;
--- test/smoke/initial.js
+++ test/smoke/initial.js
@@ -1,5 +1,8 @@
 const sel = require('../../data/selectors.json');
 const exp = require('../../data/expected.json');
+import field from "../../data/helpers";
+//const image = require('../../data/2.png');
+const path = require('path');
 
 
 
@@ -9,10 +12,9 @@ describe('My Little Hero', function () {
     describe('Getting to the page', function () {
 
         it('TC-001 Title is correct ', function () {
-            browser.url('https://qa-apps.netlify.app/app_my_hero');
-            browser.maximizeWindow()
+            browser.maximizeWindow();
             browser.deleteAllCookies();
-            browser.dele
+            browser.url('https://qa-apps.netlify.app/app_my_hero');
             let title = browser.getTitle();
             expect(title).toEqual('MLH trial');
         });
@@ -32,7 +34,7 @@ describe('My Little Hero', function () {
         });
 
         it('TC-002 Label for name is displayed', function () {
-            const label = $$(sel.label)[0].isDisplayed();
+            const label = $$(sel.label)[field.name].isDisplayed();
             expect(label).toEqual(true);
         });
 
@@ -42,27 +44,27 @@ describe('My Little Hero', function () {
         });
 
         it('TC-003 Label for gender is displayed', function () {
-            const label = $$(sel.label)[1].isDisplayed();
+            const label = $$(sel.label)[field.gender].isDisplayed();
             expect(label).toEqual(true);
         });
 
         it('#### Radio button "he" is displayed', function () {
-            const btn = $$(sel.radioBtn)[0].isDisplayed();
+            const btn = $$(sel.radioBtn)[field.HE].isDisplayed();
             expect(btn).toEqual(true);
         });
 
         it('#### Radio button "she" is displayed', function () {
-            const btn = $$(sel.radioBtn)[1].isDisplayed();
+            const btn = $$(sel.radioBtn)[field.SHE].isDisplayed();
             expect(btn).toEqual(true);
         });
 
         it('#### Radio button "it" is displayed', function () {
-            const btn = $$(sel.radioBtn)[2].isDisplayed();
+            const btn = $$(sel.radioBtn)[field.IT].isDisplayed();
             expect(btn).toEqual(true);
         });
 
         it('TC-004 Label for age is displayed', function () {
-            const label = $$(sel.label)[2].isDisplayed();
+            const label = $$(sel.label)[field.age].isDisplayed();
             expect(label).toEqual(true);
         });
 
@@ -72,7 +74,7 @@ describe('My Little Hero', function () {
         });
 
         it('TC-005 Label for story is displayed', function () {
-            const label = $$(sel.label)[3].isDisplayed();
+            const label = $$(sel.label)[field.story].isDisplayed();
             expect(label).toEqual(true);
         });
 
@@ -82,7 +84,7 @@ describe('My Little Hero', function () {
         });
 
         it('TC-006 Label for image upload is displayed', function () {
-            const label = $$(sel.label)[4].isDisplayed();
+            const label = $$(sel.label)[field.image].isDisplayed();
             expect(label).toEqual(true);
         });
 
@@ -109,8 +111,8 @@ describe('My Little Hero', function () {
             expect(text).toEqual(exp.labelSubHeader);
         });
 
-        it.skip("TC-007 Label for name = 1. What is your Hero's name?", function () {
-            const text = $$(sel.label)[0].getText();
+        it("TC-007 Label for name = 1. What is your Hero's name?", function () {
+            const text = $$(sel.label)[field.name].getText();
             expect(text).toEqual(exp.labelName);
         });
 
@@ -119,28 +121,28 @@ describe('My Little Hero', function () {
             expect(text).toEqual(exp.NamePlaceholder);
         });
 
-        it.skip('TC-008 Label for gender name = 2. Please choose a gender.', function () {
-            const text = $$(sel.label)[1].getText();
+        it('TC-008 Label for gender name = 2. Please choose a gender.', function () {
+            const text = $$(sel.label)[field.gender].getText();
             expect(text).toEqual(exp.labelGender);
         });
 
         it('Check radio button HE = he', function () {
-            const text = $$(sel.radioBtnText)[0].getText();
+            const text = $$(sel.radioBtnText)[field.HE].getText();
             expect(text).toEqual(exp.HE);
         });
 
         it('Check radio button SHE = she', function () {
-            const text = $$(sel.radioBtnText)[1].getText();
+            const text = $$(sel.radioBtnText)[field.SHE].getText();
             expect(text).toEqual(exp.SHE);
         });
 
         it('Check radio button IT = it', function () {
-            const text = $$(sel.radioBtnText)[2].getText();
+            const text = $$(sel.radioBtnText)[field.IT].getText();
             expect(text).toEqual(exp.IT);
         });
 
-        it.skip('TC-009 Label for age name = 3. How old is your Hero?', function () {
-            const text = $$(sel.label)[2].getText();
+        it('TC-009 Label for age name = 3. How old is your Hero?', function () {
+            const text = $$(sel.label)[field.age].getText();
             expect(text).toEqual(exp.labelAge);
         });
 
@@ -149,7 +151,7 @@ describe('My Little Hero', function () {
         });
 
         it('TC-0010 Label for story name = 4. What type of story would you like to read?', function () {
-            expect($$(sel.label)[3].getText()).toEqual(exp.labelStory);
+            expect($$(sel.label)[field.story].getText()).toEqual(exp.labelStory);
         });
 
         it('##### Text in 4. What type of story would you like to read? placeholder = Type of the story', function () {
@@ -157,51 +159,81 @@ describe('My Little Hero', function () {
         });
 
         // failed because "." is missing
-        it.skip('TC-0011 Label for upload an image name = 5. Upload an image (optional).', function () {
-            expect($$(sel.label)[4].getText()).toEqual(exp.labelImage);
+        it('TC-0011 Label for upload an image name = 5. Upload an image (optional).', function () {
+            expect($$(sel.label)[field.image].getText()).toEqual(exp.labelImage);
         });
 
         it('##### Text in field click or drag and drop = Click or drag and drop', function () {
             expect($(sel.dragAndDropField).getText()).toEqual(exp.textInDragAndDrop);
         });
 
+        it('##### Text in submit button  = Create!', function () {
+            expect($(sel.submitButton).getText()).toEqual(exp.textSubmitButton);
+        });
+
     });
     describe("Verify story type names in dropdown menu", function (){
         before(() => {
             $(sel.dropdownSelections).click();
-        })
+        });
 
         it('##### Verify story type dropdown menu contains = Overcoming the Monster', function () {
-            expect($$(sel.dropdownStory)[0].getText()).toEqual(exp.story1);
+            expect($$(sel.dropdownStory)[field.overcomingTheMonster].getText()).toEqual(exp.story1);
         });
 
         it('##### Verify story type dropdown menu contains = Rebirth', function () {
-            expect($$(sel.dropdownStory)[1].getText()).toEqual(exp.story2);
+            expect($$(sel.dropdownStory)[field.rebirth].getText()).toEqual(exp.story2);
         });
 
         it('##### Verify story type dropdown menu contains = Quest', function () {
-            expect($$(sel.dropdownStory)[2].getText()).toEqual(exp.story3);
+            expect($$(sel.dropdownStory)[field.quest].getText()).toEqual(exp.story3);
         });
 
         it('##### Verify story type dropdown menu contains = Journey and Return', function () {
-            expect($$(sel.dropdownStory)[3].getText()).toEqual(exp.story4);
+            expect($$(sel.dropdownStory)[field.journeyAndReturn].getText()).toEqual(exp.story4);
         });
 
         it('##### Verify story type dropdown menu contains = Rags and Riches', function () {
-            expect($$(sel.dropdownStory)[4].getText()).toEqual(exp.story5);
+            expect($$(sel.dropdownStory)[field.ragsAndRiches].getText()).toEqual(exp.story5);
         });
 
         it('##### Verify story type dropdown menu contains = Tragedy', function () {
-            expect($$(sel.dropdownStory)[5].getText()).toEqual(exp.story6);
+            expect($$(sel.dropdownStory)[field.tragedy].getText()).toEqual(exp.story6);
         });
 
         it('##### Verify story type dropdown menu contains = Comedy', function () {
-            expect($$(sel.dropdownStory)[6].getText()).toEqual(exp.story7);
+            expect($$(sel.dropdownStory)[field.comedy].getText()).toEqual(exp.story7);
         });
-        after(() => {
+    });
+
+    describe("Create a story", ()=>{
+        it("Should create a story with required fields only ", () =>{
+            $(sel.name).setValue("Nik");
+            $$(sel.radioBtn)[field.HE].click();
+            $(sel.age).setValue(123);
             $(sel.dropdownSelections).click();
-        })
-    })
+            $$(sel.dropdownStory)[field.overcomingTheMonster].click();
+            $(sel.submitButton).click();
+            expect($(sel.storyPage).isDisplayed()).toEqual(true);
+            $(sel.tryAgainBtn).click();
+
+        });
+
+        it.skip("Should create a story with all fields", () =>{
+            $(sel.name).setValue("Nik");
+            $$(sel.radioBtn)[field.HE].click();
+            $(sel.age).setValue(123);
+            $(sel.dropdownSelections).click();
+            $$(sel.dropdownStory)[field.overcomingTheMonster].click();
+            //$(sel.dragAndDropField).click();
+            //browser.uploadFile(path.join(__dirname, '../../data/2.png'));
+            $(sel.dragAndDropField).setValue(browser.uploadFile(path.join(__dirname,'../../data/2.jpg')))
+
+            $(sel.submitButton).click();
+            browser.pause(1000);
+            expect($(sel.storyPage).isDisplayed()).toEqual(true);
+        });
+    });
 
 
 });
