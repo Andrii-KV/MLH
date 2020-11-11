33a0a14d815a941f04ab721845f4cf929a9bcf6a
gennadygortalov <ggortalov@yahoo.com>
2020-11-11 13:05:44 -0500

    "regression numbered & config organized"

--- test/regression/age.js
+++ test/regression/age.js
@@ -8,42 +8,41 @@ describe('MLH-1 AGE REGRESSION', () => {
     browser.url('');
     browser.maximizeWindow();
   });
-  beforeEach(() => {
-    browser.refresh();
+
+  it('MLH-1.1 Check that we able to change the value +1 by clicking spinner up', function () {
+    $(sel.age).setValue(ageValue);
+    $(sel.spinnerAgeUp).click();
+    expect($(sel.age).getValue()).toEqual(ageValue + 1 + '');
   });
 
-  describe('MLH-1.1 AGE POSITIVE', () => {
-    it('MLH-1.1.0 Check that we able to change the value +1 by clicking spinner up', function () {
-      $(sel.age).setValue(ageValue);
-      $(sel.spinnerAgeUp).click();
-      expect($(sel.age).getValue()).toEqual(ageValue + 1 + '');
-    });
+  it('MLH-1.2 Check that we able to change the value -1 by clicking spinner down', function () {
+    $(sel.age).setValue(ageValue);
+    $(sel.spinnerAgeDown).click();
+    expect($(sel.age).getValue()).toEqual(ageValue - 1 + '');
+  });
 
-    it('Check that we able to change the value -1 by clicking spinner down', function () {
-      $(sel.age).setValue(ageValue);
-      $(sel.spinnerAgeDown).click();
-      expect($(sel.age).getValue()).toEqual(ageValue - 1 + '');
-    });
+  it('MLH-1.3 Check that get the value: 1 by clicking spinner up', function () {
+    $(sel.spinnerAgeUp).click();
+    expect($(sel.age).getValue()).toEqual(exp.one);
+  });
 
-    it('Check that get the value: 1 by clicking spinner up', function () {
-      $(sel.spinnerAgeUp).click();
-      expect($(sel.age).getValue()).toEqual(exp.one);
-    });
+  it('MLH-1.4 Verify that the age field accepts 12 symbols', function () {
+    $(sel.age).setValue(number.number3);
+    expect($(sel.errorMessage).isDisplayed()).not.toEqual(true);
+  });
 
-    it('Verify that the age field accepts 12 symbols', function () {
-      $(sel.age).setValue(number.number3);
-      expect($(sel.errorMessage).isDisplayed()).not.toEqual(true);
-    });
+  it('MLH-1.5 Verify that the age field accepts 1 digit', function () {
+    $(sel.age).setValue(number.number4);
+    expect($(sel.errorMessage).isDisplayed()).not.toEqual(true);
+  });
 
-    it('Verify that the age field accepts 1 digit', function () {
-      $(sel.age).setValue(number.number4);
-      expect($(sel.errorMessage).isDisplayed()).not.toEqual(true);
-    });
+  it('MLH-1.6 Verify that if input value is longer than a 12-digit integer, error message appears', function () {
+    $(sel.age).setValue(number.number5);
+    $(sel.errorMessage).waitForDisplayed();
+    expect($(sel.errorMessage).isDisplayed()).toEqual(true);
+  });
 
-    it('Verify that if input value is longer than a 12-digit integer, error message appears', function () {
-      $(sel.age).setValue(number.number5);
-      $(sel.errorMessage).waitForDisplayed();
-      expect($(sel.errorMessage).isDisplayed()).toEqual(true);
-    });
+  afterEach(() => {
+    browser.refresh();
   });
 });
