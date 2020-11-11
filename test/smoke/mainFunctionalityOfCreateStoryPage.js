4c18afd4f2744e65f84d9818baa8cd086dc74b5e
gennadygortalov <ggortalov@yahoo.com>
2020-11-11 13:55:40 -0500

    "regression numbered & config organized"

--- test/smoke/mainFunctionalityOfCreateStoryPage.js
+++ test/smoke/mainFunctionalityOfCreateStoryPage.js
@@ -4,24 +4,24 @@ import { number, gender, image, name, story } from '../../data/testData';
 const sel = require('../../data/selectors.json');
 const exp = require('../../data/expected.json');
 
-describe('MAIN FUNCTIONALITY', () => {
+describe('MLH-4 MAIN FUNCTIONALITY', () => {
   before(() => {
     browser.url('');
     browser.maximizeWindow();
   });
 
-  describe('CREATE A STORY', () => {
-    it('should create a story with required fields only', function () {
+  describe('MLH-4.1 CREATE A STORY', () => {
+    it('MLH-4.1.1.0 should create a story with required fields only', function () {
       inputValues4AndClick(name.userHe, gender.HE, number.age, story.comedy);
       expect($(sel.storyPage).isDisplayed()).toEqual(true);
     });
 
-    it('should create a story with all fields (*.jpg)', function () {
+    it('MLH-4.1.1 should create a story with all fields (*.jpg)', function () {
       inputValues5AndClick(name.userHe, gender.HE, number.age, story.comedy, image.testImageJpg);
       expect($(sel.storyPage).isDisplayed()).toEqual(true);
     });
 
-    it('should create a story with all fields (*.png)', function () {
+    it('MLH-4.1.2 should create a story with all fields (*.png)', function () {
       inputValues5AndClick(name.userHe, gender.HE, number.age, story.comedy, image.testImagePng);
       expect($(sel.storyPage).isDisplayed()).toEqual(true);
     });
@@ -32,35 +32,35 @@ describe('MAIN FUNCTIONALITY', () => {
     });
   });
 
-  describe('SUBMIT BUTTON', () => {
-    it('SUBMIT button is INACTIVE by default', () => {
+  describe('MLH-4.2 SUBMIT BUTTON', () => {
+    it('MLH-4.2.0 SUBMIT button is INACTIVE by default', () => {
       expect($(sel.submitButton).isClickable()).toEqual(false);
     });
 
-    it('SUBMIT button is INACTIVE with NAME field filled', () => {
+    it('MLH-4.2.1 SUBMIT button is INACTIVE with NAME field filled', () => {
       $(sel.name).setValue(name.userShe);
       expect($(sel.submitButton).isClickable()).toEqual(false);
     });
 
-    it('SUBMIT button is INACTIVE with NAME,GENDER field filled', () => {
+    it('MLH-4.2.2 SUBMIT button is INACTIVE with NAME,GENDER field filled', () => {
       $(sel.name).setValue(name.userHe);
       $$(sel.gender)[gender.HE].click();
       expect($(sel.submitButton).isClickable()).toEqual(false);
     });
 
-    it('SUBMIT button is INACTIVE with NAME,GENDER,AGE field filled', () => {
+    it('MLH-4.2.3 SUBMIT button is INACTIVE with NAME,GENDER,AGE field filled', () => {
       $(sel.name).setValue(name.userHe);
       $$(sel.gender)[gender.HE].click();
       $(sel.age).setValue(number.age);
       expect($(sel.submitButton).isClickable()).toEqual(false);
     });
 
-    it('SUBMIT Button is ACTIVE with REQUIRED fields filled up', () => {
+    it('MLH-4.2.4 SUBMIT Button is ACTIVE with REQUIRED fields filled up', () => {
       inputValues4(name.userHe, gender.HE, number.age, story.comedy);
       expect($(sel.submitButton).isClickable()).toEqual(true);
     });
 
-    it('SUBMIT Button is ACTIVE with ALL fields filled up', () => {
+    it('MLH-4.2.5 SUBMIT Button is ACTIVE with ALL fields filled up', () => {
       inputValues5(name.userHe, gender.HE, number.age, story.comedy, image.testImageJpg);
       expect($(sel.submitButton).isClickable()).toEqual(true);
     });
@@ -70,69 +70,69 @@ describe('MAIN FUNCTIONALITY', () => {
     });
   });
 
-  describe('ERROR MESSAGE', () => {
+  describe('MLH-4.3 ERROR MESSAGE', () => {
     afterEach(() => {
       browser.refresh();
     });
 
-    it('should check that ERROR MESSAGE displayed when name field empty', function () {
+    it('MLH-4.3.0 should check that ERROR MESSAGE displayed when name field empty', function () {
       $(sel.name).setValue(name.userIt);
       clearInputBox(sel.name);
       $(sel.errorMessage).waitForDisplayed();
       expect($(sel.errorMessage).isDisplayed()).toEqual(true);
     });
 
-    it('should check that ERROR MESSAGE = "Required" when name field is empty', function () {
+    it('MLH-4.3.1 should check that ERROR MESSAGE = "Required" when name field is empty', function () {
       $(sel.name).setValue(name.userIt);
       clearInputBox(sel.name);
       $(sel.errorMessage).waitForDisplayed();
       expect($(sel.errorMessage).getText()).toEqual(exp['errorMSG.Required']);
     });
 
-    it('should check that ERROR MESSAGE displayed when name field over 71 symbols', function () {
+    it('MLH-4.3.2 should check that ERROR MESSAGE displayed when name field over 71 symbols', function () {
       $(sel.name).setValue(name.user71Symbols);
       $(sel.errorMessage).waitForDisplayed();
       expect($(sel.errorMessage).isDisplayed()).toEqual(true);
     });
 
-    it('should check that ERROR MESSAGE = "70 symbols max" when user enter name bigger than 70 symbols', function () {
+    it('MLH-4.3.3 should check that ERROR MESSAGE = "70 symbols max" when user enter name bigger than 70 symbols', function () {
       $(sel.name).setValue(name.user71Symbols);
       $(sel.errorMessage).waitForDisplayed();
       expect($(sel.errorMessage).getText()).toEqual(exp['errorMSG.70SymbolsMax']);
     });
 
-    it('should check that ERROR MESSAGE displayed when clicking age spinner down', function () {
+    it('MLH-4.3.4 should check that ERROR MESSAGE displayed when clicking age spinner down', function () {
       $(sel.spinnerAgeDown).click();
       $(sel.errorMessage).waitForDisplayed();
       expect($(sel.errorMessage).isDisplayed()).toEqual(true);
     });
 
-    it('should check that ERROR MESSAGE = "looks like unreal age" when clicking age spinner down', function () {
+    it('MLH-4.3.5 should check that ERROR MESSAGE = "looks like unreal age" when clicking age spinner down', function () {
       $(sel.spinnerAgeDown).click();
       $(sel.errorMessage).waitForDisplayed();
       expect($(sel.errorMessage).getText()).toEqual(exp['errorMSG.UnrealAge']);
     });
 
-    it('should check that ERROR MESSAGE displayed when input -10 in age field', function () {
+    it('MLH-4.3.6 should check that ERROR MESSAGE displayed when input -10 in age field', function () {
       $(sel.age).setValue(number.number2);
       $(sel.errorMessage).waitForDisplayed();
       expect($(sel.errorMessage).isDisplayed()).toEqual(true);
     });
 
-    it('should check that ERROR MESSAGE = "looks like unreal age" when input -10 in age field', function () {
+    it('MLH-4.3.7 should check that ERROR MESSAGE = "looks like unreal age" when input -10 in age field', function () {
       $(sel.age).setValue(number.number2);
       $(sel.errorMessage).waitForDisplayed();
       expect($(sel.errorMessage).getText()).toEqual(exp['errorMSG.UnrealAge']);
     });
 
-    it('should check that ERROR MESSAGE displayed when clicking age spinner down and clear input', function () {
+    it('MLH-4.3.8 should check that ERROR MESSAGE displayed when clicking age spinner down and clear input', function () {
       $(sel.spinnerAgeDown).click();
       clearInputBox(sel.age);
       $(sel.errorMessage).waitForDisplayed();
       expect($(sel.errorMessage).isDisplayed()).toEqual(true);
     });
 
-    it('should check that ERROR MESSAGE = "Required" when clicking age spinner down and clear input', function () {
+    it('MLH-4.3.9 should check that ERROR MESSAGE = "Required" when clicking age spinner down and clear input', function () {
       $(sel.spinnerAgeDown).click();
       clearInputBox(sel.age);
       browser.keys('Backspace');
@@ -140,7 +140,7 @@ describe('MAIN FUNCTIONALITY', () => {
       expect($(sel.errorMessage).getText()).toEqual(exp['errorMSG.Required']);
     });
 
-    it('should check that ERROR MESSAGE displayed when input -10 in age field and clear input', function () {
+    it('MLH-4.3.10 should check that ERROR MESSAGE displayed when input -10 in age field and clear input', function () {
       $(sel.age).setValue(number.number2);
       clearInputBox(sel.age);
       browser.keys('Backspace');
@@ -148,7 +148,7 @@ describe('MAIN FUNCTIONALITY', () => {
       expect($(sel.errorMessage).isDisplayed()).toEqual(true);
     });
 
-    it('should check that ERROR MESSAGE = "Required" when input -10 in age field and clear input', function () {
+    it('MLH-4.3.11 should check that ERROR MESSAGE = "Required" when input -10 in age field and clear input', function () {
       $(sel.age).setValue(number.number2);
       clearInputBox(sel.age);
       browser.keys('Backspace');
@@ -156,75 +156,75 @@ describe('MAIN FUNCTIONALITY', () => {
       expect($(sel.errorMessage).getText()).toEqual(exp['errorMSG.Required']);
     });
 
-    it('should throw an error while trying to upload the PDF file', () => {
+    it('MLH-4.3.12 should throw an error while trying to upload the PDF file', () => {
       inputValues5(name.userIt, gender.IT, number.age, story.journeyAndReturn, image.testImagePdf);
       $(sel.errorMessageImage).waitForDisplayed();
       expect($(sel.errorMessageImage).isDisplayed()).toEqual(true);
     });
 
-    it("error message should be = 'You can only upload JPG/PNG file!' while trying to upload the PDF file", () => {
+    it("MLH-4.3.13 error message should be = 'You can only upload JPG/PNG file!' while trying to upload the PDF file", () => {
       inputValues5(name.userIt, gender.IT, number.age, story.journeyAndReturn, image.testImagePdf);
       $(sel.errorMessageImage).waitForDisplayed();
       expect($(sel.errorMessageImage).getText()).toEqual(exp.errorMessageImage);
     });
 
-    it('should throw an error while trying to upload the JPG file 4.1 MB', () => {
+    it('MLH-4.3.14 should throw an error while trying to upload the JPG file 4.1 MB', () => {
       inputValues5(name.userIt, gender.IT, number.age, story.journeyAndReturn, image.testImageJpg41);
       $(sel.errorMessageImage).waitForDisplayed();
       expect($(sel.errorMessageImage).isDisplayed()).toEqual(true);
     });
 
-    it("error message should be = 'Image must be smaller than 4MB!' while trying to upload the JPG over 4.1 MB file", () => {
+    it("MLH-4.3.15 error message should be = 'Image must be smaller than 4MB!' while trying to upload the JPG over 4.1 MB file", () => {
       inputValues5(name.userIt, gender.IT, number.age, story.journeyAndReturn, image.testImageJpg41);
       $(sel.errorMessageImage).waitForDisplayed();
       expect($(sel.errorMessageImage).getText()).toEqual(exp.errorMessageImageSmaller4Mb);
     });
   });
 
-  describe('TYPE OF STORY SELECTED', () => {
+  describe('MLH-4.4 TYPE OF STORY SELECTED', () => {
     beforeEach(() => {
       browser.refresh();
       $(sel.dropdownSelections).click();
       $(sel.dropdownMenu).waitForDisplayed();
     });
 
-    it('##### Verify that selected story type is displayed = Overcoming the Monster', () => {
+    it('MLH-4.4.0 Verify that selected story type is displayed = Overcoming the Monster', () => {
       $$(sel.storyPositionInDropdown)[story.overcomingTheMonster].click();
       $(sel.selectedStoryType).waitForDisplayed();
       expect($(sel.selectedStoryType).getText()).toEqual(exp.OvercomingTheMonster);
     });
 
-    it('##### Verify that selected story type is displayed = Rebirth', () => {
+    it('MLH-4.4.1 Verify that selected story type is displayed = Rebirth', () => {
       $$(sel.storyPositionInDropdown)[story.rebirth].click();
       $(sel.selectedStoryType).waitForDisplayed();
       expect($(sel.selectedStoryType).getText()).toEqual(exp.Rebirth);
     });
 
-    it('##### Verify that selected story type is displayed = Quest', () => {
+    it('MLH-4.4.2 Verify that selected story type is displayed = Quest', () => {
       $$(sel.storyPositionInDropdown)[story.quest].click();
       $(sel.selectedStoryType).waitForDisplayed();
       expect($(sel.selectedStoryType).getText()).toEqual(exp.Quest);
     });
 
-    it('##### Verify that selected story type is displayed = Journey and Return', () => {
+    it('MLH-4.4.3 Verify that selected story type is displayed = Journey and Return', () => {
       $$(sel.storyPositionInDropdown)[story.journeyAndReturn].click();
       $(sel.selectedStoryType).waitForDisplayed();
       expect($(sel.selectedStoryType).getText()).toEqual(exp.JourneyAndReturn);
     });
 
-    it('##### Verify that selected story type is displayed = Rags and Riches', () => {
+    it('MLH-4.4.4 Verify that selected story type is displayed = Rags and Riches', () => {
       $$(sel.storyPositionInDropdown)[story.ragsAndRiches].click();
       $(sel.selectedStoryType).waitForDisplayed();
       expect($(sel.selectedStoryType).getText()).toEqual(exp.RagsAndRiches);
     });
 
-    it('##### Verify that selected story type is displayed = Tragedy', () => {
+    it('MLH-4.4.5 Verify that selected story type is displayed = Tragedy', () => {
       $$(sel.storyPositionInDropdown)[story.tragedy].click();
       $(sel.selectedStoryType).waitForDisplayed();
       expect($(sel.selectedStoryType).getText()).toEqual(exp.Tragedy);
     });
 
-    it('##### Verify that selected story type is displayed = Comedy', () => {
+    it('MLH-4.4.6 Verify that selected story type is displayed = Comedy', () => {
       $$(sel.storyPositionInDropdown)[story.comedy].click();
       $(sel.selectedStoryType).waitForDisplayed();
       expect($(sel.selectedStoryType).getText()).toEqual(exp.Comedy);
--- test/smoke/text.js
+++ test/smoke/text.js
@@ -2,107 +2,107 @@ import { field, gender, story } from '../../data/testData';
 const sel = require('../../data/selectors.json');
 const exp = require('../../data/expected.json');
 
-describe('VERIFY TEXT IN LABELS, PLACEHOLDERS ARE CORRECT', () => {
+describe('MLH-6 VERIFY TEXT IN LABELS, PLACEHOLDERS ARE CORRECT', () => {
   before(() => {
     browser.url('');
     browser.maximizeWindow();
   });
 
-  it('TC-000 Title is correct ', () => {
+  it('MLH-6.1.0 Title is correct ', () => {
     expect(browser.getTitle()).toEqual(exp.title);
   });
 
-  it('TC-001 Label for HEADER = My Little Hero', () => {
+  it('MLH-6.1.1 Label for HEADER = My Little Hero', () => {
     expect($(sel.header).getText()).toEqual(exp.labelHeader);
   });
 
-  it("TC-002 Label for subHEADER = Let's create your own HERO! It's super easy with our application!", () => {
+  it("MLH-6.1.2  Label for subHEADER = Let's create your own HERO! It's super easy with our application!", () => {
     expect($(sel.subHeader).getText()).toEqual(exp.labelSubHeader);
   });
 
-  it("TC-003 Label for name = 1. What is your Hero's name?", () => {
+  it("MLH-6.1.3 Label for name = 1. What is your Hero's name?", () => {
     expect($$(sel.label)[field.name].getText()).toEqual(exp.labelName);
   });
 
-  it("TC-004  Text in Hero's name placeholder = Hero's name", () => {
+  it("MLH-6.1.4  Text in Hero's name placeholder = Hero's name", () => {
     expect($(sel.name).getAttribute('placeholder')).toEqual(exp.NamePlaceholder);
   });
 
-  it('TC-005 Label for gender name = 2. Please choose a gender.', () => {
+  it('MLH-6.1.5 Label for gender name = 2. Please choose a gender.', () => {
     expect($$(sel.label)[field.gender].getText()).toEqual(exp.labelGender);
   });
 
-  it('TC-006 Check radio button HE = he', () => {
+  it('MLH-6.1.6 Check radio button HE = he', () => {
     expect($$(sel.radioBtnText)[gender.HE].getText()).toEqual(exp.HE);
   });
 
-  it('TC-007 Check radio button SHE = she', () => {
+  it('MLH-6.1.7 Check radio button SHE = she', () => {
     expect($$(sel.radioBtnText)[gender.SHE].getText()).toEqual(exp.SHE);
   });
 
-  it('TC-008 Check radio button IT = it', () => {
+  it('MLH-6.1.8 Check radio button IT = it', () => {
     expect($$(sel.radioBtnText)[gender.IT].getText()).toEqual(exp.IT);
   });
 
-  it('TC-009 Label for age name = 3. How old is your Hero?', () => {
+  it('MLH-6.1.9 Label for age name = 3. How old is your Hero?', () => {
     expect($$(sel.label)[field.age].getText()).toEqual(exp.labelAge);
   });
 
-  it("TC-010 Text in 3. How old is your hero? placeholder = Hero's age", () => {
+  it("MLH-6.1.10 Text in 3. How old is your hero? placeholder = Hero's age", () => {
     expect($(sel.age).getAttribute('placeholder')).toEqual(exp.AgePlaceholder);
   });
 
-  it('TC-011 Label for story name = 4. What type of story would you like to read?', () => {
+  it('MLH-6.1.11 Label for story name = 4. What type of story would you like to read?', () => {
     expect($$(sel.label)[field.story].getText()).toEqual(exp.labelStory);
   });
 
-  it('TC-012 Text in 4. What type of story would you like to read? placeholder = Type of the story', () => {
+  it('MLH-6.1.12 Text in 4. What type of story would you like to read? placeholder = Type of the story', () => {
     expect($(sel.story).getText()).toEqual(exp.storyPlaceholder);
   });
 
-  it('TC-013 Label for upload an image name = 5. Upload an image (optional).', () => {
+  it('MLH-6.1.13 Label for upload an image name = 5. Upload an image (optional).', () => {
     expect($$(sel.label)[field.image].getText()).toEqual(exp.labelImage);
   });
 
-  it('TC-014 Text in field click or drag and drop = Click or drag and drop', () => {
+  it('MLH-6.1.14 Text in field click or drag and drop = Click or drag and drop', () => {
     expect($(sel.dragAndDropField).getText()).toEqual(exp.textInDragAndDrop);
   });
 
-  it('TC-015 Text in submit button = Create!', () => {
+  it('MLH-6.1.15 Text in submit button = Create!', () => {
     expect($(sel.submitButton).getText()).toEqual(exp.textSubmitButton);
   });
 
-  describe('Verify story type names in dropdown menu', () => {
+  describe('MLH-6.2 Verify story type names in dropdown menu', () => {
     before(() => {
       $(sel.dropdownSelections).click();
       $(sel.dropdownMenu).waitForDisplayed();
     });
 
-    it('##### Verify story type dropdown menu contains = Overcoming the Monster', () => {
+    it('MLH-6.2.0 Verify story type dropdown menu contains = Overcoming the Monster', () => {
       expect($$(sel.storyPositionInDropdown)[story.overcomingTheMonster].getText()).toEqual(exp.OvercomingTheMonster);
     });
 
-    it('##### Verify story type dropdown menu contains = Rebirth', () => {
+    it('MLH-6.2.1 Verify story type dropdown menu contains = Rebirth', () => {
       expect($$(sel.storyPositionInDropdown)[story.rebirth].getText()).toEqual(exp.Rebirth);
     });
 
-    it('##### Verify story type dropdown menu contains = Quest', () => {
+    it('MLH-6.2.2 Verify story type dropdown menu contains = Quest', () => {
       expect($$(sel.storyPositionInDropdown)[story.quest].getText()).toEqual(exp.Quest);
     });
 
-    it('##### Verify story type dropdown menu contains = Journey and Return', () => {
+    it('MLH-6.2.3 Verify story type dropdown menu contains = Journey and Return', () => {
       expect($$(sel.storyPositionInDropdown)[story.journeyAndReturn].getText()).toEqual(exp.JourneyAndReturn);
     });
 
-    it('##### Verify story type dropdown menu contains = Rags and Riches', () => {
+    it('MLH-6.2.4 Verify story type dropdown menu contains = Rags and Riches', () => {
       expect($$(sel.storyPositionInDropdown)[story.ragsAndRiches].getText()).toEqual(exp.RagsAndRiches);
     });
 
-    it('##### Verify story type dropdown menu contains = Tragedy', () => {
+    it('MLH-6.2.5 Verify story type dropdown menu contains = Tragedy', () => {
       expect($$(sel.storyPositionInDropdown)[story.tragedy].getText()).toEqual(exp.Tragedy);
     });
 
-    it('##### Verify story type dropdown menu contains = Comedy', () => {
+    it('MLH-6.2.6 Verify story type dropdown menu contains = Comedy', () => {
       expect($$(sel.storyPositionInDropdown)[story.comedy].getText()).toEqual(exp.Comedy);
     });
   });
