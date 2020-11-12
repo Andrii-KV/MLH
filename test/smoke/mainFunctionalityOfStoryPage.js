ad49f128f57df0235225c79d360abb61d28b6495
Gennady Gortalov <60904111+gennadyGOR@users.noreply.github.com>
2020-11-12 01:32:54 -0500

    New (#18)
    * "regression numbered & config organized"
    
    * "regression numbered & config organized"
    
    Co-authored-by: gennadygortalov <ggortalov@yahoo.com>

--- test/smoke/mainFunctionalityOfStoryPage.js
+++ test/smoke/mainFunctionalityOfStoryPage.js
@@ -12,58 +12,60 @@ import { number, gender, image, name, story } from '../../data/testData';
 const sel = require('../../data/selectors.json');
 const exp = require('../../data/expected.json');
 
-describe('STORY PAGE FUNCTIONALITY', () => {
+describe('MLH-5 STORY PAGE FUNCTIONALITY', () => {
   before(() => {
     browser.url('');
     browser.maximizeWindow();
   });
 
-  describe('IMAGE ON THE STORY PAGE', () => {
-    it('VERIFY THAT STORY PAGE HAS NO IMAGE BY DEFAULT', () => {
+  describe('MLH-5.1 IMAGE ON THE STORY PAGE', () => {
+
+    it('MLH-5.1.0 VERIFY THAT STORY PAGE HAS NO IMAGE BY DEFAULT', () => {
       inputValues4AndClick(name.userHe, gender.HE, number.age, story.comedy);
       expect($(sel.storyImage).getAttribute('src')).toEqual(exp.defaultImageArea);
       $(sel.tryAgainBtn).click();
     });
 
-    it('VERIFY THAT UPLOADED IMAGE IS VISIBLE', () => {
+    it('MLH-5.1.1 VERIFY THAT UPLOADED IMAGE IS VISIBLE', () => {
       inputValues5AndClick(name.userHe, gender.HE, number.age, story.comedy, image.testImageJpg);
       expect($(sel.storyImage).getAttribute('src')).not.toEqual(exp.defaultImageArea);
       $(sel.tryAgainBtn).click();
     });
   });
 
-  describe('NAME, GENDER, AGE OF A THE STORY PAGE', () => {
-    it('VERIFY THAT SUBHEADER CONTAINS OUR HERO NAME = "Nick" ', () => {
+  describe('MLH-5.2 NAME, GENDER, AGE OF A THE STORY PAGE', () => {
+
+    it('MLH-5.2.0 VERIFY THAT SUBHEADER CONTAINS OUR HERO NAME = "Nick" ', () => {
       inputValues4AndClick(name.userHe, gender.HE, number.age, story.comedy);
       expect($(sel.subHeaderStory).getText().includes(name.userHe)).toEqual(true);
     });
 
-    it('VERIFY THAT SUBHEADER CONTAINS OUR HERO NAME = "Nick" IN THE RIGHT PLACE', () => {
+    it('MLH-5.2.1 VERIFY THAT SUBHEADER CONTAINS OUR HERO NAME = "Nick" IN THE RIGHT PLACE', () => {
       inputValues4AndClick(name.userHe, gender.HE, number.age, story.comedy);
       expect(nameInStorySubHeader(name.userHe)).toEqual(exp.userNameHe);
     });
 
-    it('VERIFY THAT STORY GENERATED WITH OUR HERO NAME = "Nick" ', () => {
+    it('MLH-5.2.2 VERIFY THAT STORY GENERATED WITH OUR HERO NAME = "Nick" ', () => {
       inputValues4AndClick(name.userHe, gender.HE, number.age, story.comedy);
       expect(nameInComedyStory(name.userHe)).toEqual(exp.userNameHe);
     });
 
-    it('VERIFY THAT STORY CREATED WITH GENDER = HE in body of a story', () => {
+    it('MLH-5.2.3 VERIFY THAT STORY CREATED WITH GENDER = HE in body of a story', () => {
       inputValues4AndClick(name.userHe, gender.HE, number.age, story.comedy);
       expect(genderInComedyStory(number.age)).toEqual(exp.genderStoryHe);
     });
 
-    it('VERIFY THAT STORY CREATED WITH GENDER = SHE in body of a story', () => {
+    it('MLH-5.2.4 VERIFY THAT STORY CREATED WITH GENDER = SHE in body of a story', () => {
       inputValues4AndClick(name.userShe, gender.SHE, number.age, story.comedy);
       expect(genderInComedyStory(number.age)).toEqual(exp.genderStoryShe);
     });
 
-    it('VERIFY THAT STORY CREATED WITH GENDER = IT in body of a story', () => {
+    it('MLH-5.2.5 VERIFY THAT STORY CREATED WITH GENDER = IT in body of a story', () => {
       inputValues4AndClick(name.userIt, gender.IT, number.age, story.comedy);
       expect(genderInComedyStory(number.age)).toEqual(exp.genderStoryIt);
     });
 
-    it('VERIFY THAT AGE of a hero = "fifty" in a body of a CREATED STORY', () => {
+    it('MLH-5.2.6 VERIFY THAT AGE of a hero = "fifty" in a body of a CREATED STORY', () => {
       inputValues4AndClick(name.userIt, gender.IT, number.age, story.comedy);
       expect(ageInTheStoryText()).toEqual(numToText(number.age));
     });
