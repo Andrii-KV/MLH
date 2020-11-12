e47e517017c52db7e76e910885e2bca26476c0d3
Gennady Gortalov <60904111+gennadyGOR@users.noreply.github.com>
2020-11-12 01:23:21 -0500

    "regression numbered & config organized" (#17)
    Co-authored-by: gennadygortalov <ggortalov@yahoo.com>

--- test/regression/name.js
+++ test/regression/name.js
@@ -2,82 +2,83 @@ import { name } from '../../data/testData';
 const sel = require('../../data/selectors.json');
 const exp = require('../../data/expected.json');
 
-describe('NAME REGRESSION', () => {
+describe('MLH-2 NAME REGRESSION', () => {
   before(() => {
     browser.url('');
     browser.maximizeWindow();
   });
-  beforeEach(() => {
-    browser.refresh();
-  });
 
-  it('input name one Symbol', function () {
+  it('MLH-2.1 input name one Symbol', function () {
     $(sel.name).setValue(name.userOneSymbol);
     expect($(sel.name).getValue()).toEqual(exp.userOneSymbol);
   });
 
-  it('input name one Letter', function () {
+  it('MLH-2.2 input name one Letter', function () {
     $(sel.name).setValue(name.userOneLetter);
     expect($(sel.name).getValue()).toEqual(exp.userOneLetter);
   });
 
-  it('input name one Number', function () {
+  it('MLH-2.3 input name one Number', function () {
     $(sel.name).setValue(name.userOneNumber);
     expect($(sel.name).getValue()).toEqual(exp.userOneNumber);
   });
 
-  it('input name Letters', function () {
+  it('MLH-2.4 input name Letters', function () {
     $(sel.name).setValue(name.userNameLetters);
     expect($(sel.name).getValue()).toEqual(exp.userNameLetters);
   });
 
-  it('input name Numbers', function () {
+  it('MLH-2.5 input name Numbers', function () {
     $(sel.name).setValue(name.userNameNumbers);
     expect($(sel.name).getValue()).toEqual(exp.userNameNumbers);
   });
 
-  it('input name Symbols', function () {
+  it('MLH-2.6 input name Symbols', function () {
     $(sel.name).setValue(name.userNameSymbols);
     expect($(sel.name).getValue()).toEqual(exp.userNameSymbols);
   });
 
-  it('input name Letters, Numbers, Symbols', function () {
+  it('MLH-2.7 input name Letters, Numbers, Symbols', function () {
     $(sel.name).setValue(name.userNameLetNumSym);
     expect($(sel.name).getValue()).toEqual(exp.userNameLetNumSym);
   });
 
-  it('input name to upper case Letters', function () {
+  it('MLH-2.8 input name to upper case Letters', function () {
     $(sel.name).setValue(name.userNameUpperCase);
     expect($(sel.name).getValue()).toEqual(exp.userNameUpperCase);
   });
 
-  it('input name to lower case Letters', function () {
+  it('MLH-2.9 input name to lower case Letters', function () {
     $(sel.name).setValue(name.userNameLowerCase);
     expect($(sel.name).getValue()).toEqual(exp.userNameLowerCase);
   });
 
-  it('input name to 70 Symbols', function () {
+  it('MLH-2.10 input name to 70 Symbols', function () {
     $(sel.name).setValue(name.user70Symbols);
     expect($(sel.name).getValue()).toEqual(exp.user70Symbols);
   });
 
-  it('input name to 70 Symbols NO ERROR', function () {
+  it('MLH-2.11 input name to 70 Symbols NO ERROR', function () {
     $(sel.name).setValue(name.user70Symbols);
     expect($(sel.errorMessage).isDisplayed()).toEqual(false);
   });
 
-  it('input name one Symbol NO ERROR', function () {
+  it('MLH-2.12 input name one Symbol NO ERROR', function () {
     $(sel.name).setValue(name.userOneSymbol);
     expect($(sel.errorMessage).isDisplayed()).toEqual(false);
   });
 
-  it('input name one Letter NO ERROR', function () {
+  it('MLH-2.13 input name one Letter NO ERROR', function () {
     $(sel.name).setValue(name.userOneLetter);
     expect($(sel.errorMessage).isDisplayed()).toEqual(false);
   });
 
-  it('input name one Number NO ERROR', function () {
+  it('MLH-2.14 input name one Number NO ERROR', function () {
     $(sel.name).setValue(name.userOneNumber);
     expect($(sel.errorMessage).isDisplayed()).toEqual(false);
+  });
+
+  afterEach(() => {
+    browser.refresh();
   });
 });
--- test/smoke/labels.js
+++ test/smoke/labels.js
@@ -1,76 +1,71 @@
 const sel = require('../../data/selectors.json');
 import { field, gender, story } from '../../data/testData';
 
-describe('Labels exist', () => {
+describe('MLH-3 LABELS АRЕ DISPLAYED', () => {
   before(() => {
     browser.url('');
     browser.maximizeWindow();
   });
 
-  it('TC-001 Header is displayed ', () => {
+  it('MLH-3.1 Header is displayed ', () => {
     expect($(sel.header).isDisplayed()).toEqual(true);
   });
 
-  it('TC-002 Sub header is displayed ', () => {
+  it('MLH-3.2 Sub header is displayed ', () => {
     expect($(sel.subHeader).isDisplayed()).toEqual(true);
   });
 
-  it('TC-003 Label for name is displayed', () => {
+  it('MLH-3.3 Label for name is displayed', () => {
     expect($$(sel.label)[field.name].isDisplayed()).toEqual(true);
   });
 
-  it('TC-004 Field NAME is displayed', () => {
+  it('MLH-3.4 Field NAME is displayed', () => {
     expect($(sel.name).isDisplayed()).toEqual(true);
   });
 
-  it('TC-005 Label for gender is displayed', () => {
+  it('MLH-3.5 Label for gender is displayed', () => {
     expect($$(sel.label)[field.gender].isDisplayed()).toEqual(true);
   });
 
-  it('TC-006 Radio button "he" is displayed', () => {
+  it('MLH-3.6 Radio button "he" is displayed', () => {
     expect($$(sel.radioBtn)[gender.HE].isDisplayed()).toEqual(true);
   });
 
-  it('TC-007 Radio button "she" is displayed', () => {
+  it('MLH-3.7 Radio button "she" is displayed', () => {
     expect($$(sel.radioBtn)[gender.SHE].isDisplayed()).toEqual(true);
   });
 
-  it('TC-008 Radio button "it" is displayed', () => {
+  it('MLH-3.8 Radio button "it" is displayed', () => {
     expect($$(sel.radioBtn)[gender.IT].isDisplayed()).toEqual(true);
   });
 
-  it('TC-009 Label for age is displayed', () => {
+  it('MLH-3.9 Label for age is displayed', () => {
     expect($$(sel.label)[field.age].isDisplayed()).toEqual(true);
   });
 
-  it('TC-010 Field AGE is displayed', () => {
+  it('MLH-3.10 Field AGE is displayed', () => {
     expect($(sel.age).isDisplayed()).toEqual(true);
   });
 
-  it('TC-011 Label for story is displayed', () => {
+  it('MLH-3.11 Label for story is displayed', () => {
     expect($$(sel.label)[field.story].isDisplayed()).toEqual(true);
   });
 
-  it('TC-012 Field STORY is displayed', () => {
+  it('MLH-3.12 Field STORY is displayed', () => {
     expect($(sel.story).isDisplayed()).toEqual(true);
   });
 
-  it('TC-013 Label for image upload is displayed', () => {
+  it('MLH-3.13 Label for image upload is displayed', () => {
     expect($$(sel.label)[field.image].isDisplayed()).toEqual(true);
   });
 
-  it('TC-014 Field click or drag and drop is displayed', () => {
+  it('MLH-3.14 Field click or drag and drop is displayed', () => {
     expect($(sel.dragAndDropField).isDisplayed()).toEqual(true);
   });
 
-  it('TC-015 Submit button is displayed', () => {
+  it('MLH-3.15 Submit button is displayed', () => {
     expect($(sel.submitButton).isDisplayed()).toEqual(true);
   });
 
-  it('should #######', function () {
-    $(sel.dropdownSelections).click();
-    $$(sel.storyPositionInDropdown)[story.tragedy].click();
-    expect($('.ant-select-selection-item').getText()).toEqual('Tragedy');
-    browser.pause(3000);
-  });
+  // should add same thing for drop down menu ???????????????????????????
 });
